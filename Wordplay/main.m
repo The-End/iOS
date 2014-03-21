295f5b2704b188b242126677d43e81f35f5846bf
Marshall Mann-Wood <marshallmannwood@gmail.com>
2014-03-21 11:20:16 -0500

    Added missing files

---
+++ Wordplay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wordplay
+//
+//  Created by Blake Martin on 3/21/14.
+//  Copyright (c) 2014 Blake Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
